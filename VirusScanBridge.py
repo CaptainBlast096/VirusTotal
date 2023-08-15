b46baa6e03835b47bf9cb1b267096157c1624027
Jaleel Rogers <jaleelr96@gmail.com>
2023-08-15 01:17:31 -0400

    Started working on project. *Added API Key *Directory Name In Progress: *For loop for APK directory

--- VirusScanBridge.py
+++ VirusScanBridge.py
@@ -1,2 +1,13 @@
-print ("Hello")
-print ("World")
+import virustotal_python # VirusTotal API
+import os # For file path
+
+# VirusTotal API Key
+API_KEY = '6bf043675b60ab5626d10857b580e9456e415bbff9223f88258b77b2e85dc85b'
+
+# VirusTotal Instance
+virusTotal = virustotal_python.Virustotal(API_KEY)
+
+# Since its a directory with multiple files I need to iterate through the file
+apk_directory = r'C:\Users\jalee\PycharmProjects\APK'
+for filename in os.listdir(apk_directory):
+
