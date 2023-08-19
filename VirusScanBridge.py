16c09cd7778286a297da14849e7a1fba78266201
Jaleel Rogers <jaleelr96@gmail.com>
2023-08-18 22:03:18 -0400

    Update VirusScanBridge.py
    Tried implementing the collection

--- VirusScanBridge.py
+++ VirusScanBridge.py
@@ -3,9 +3,18 @@ import os # For file path
 import requests # For HTTP requests
 from pprint import pprint # For pretty printing
 from openpyxl import Workbook # For Excel
+
 # VirusTotal API Key
 API_KEY = '6bf043675b60ab5626d10857b580e9456e415bbff9223f88258b77b2e85dc85b'
+Collection_ID = '6fbcb4121a78476a77a6982498a1368624454c08bee9b597ba5d1ab1f1a06cbb'
+upload_url = 'https://www.virustotal.com/gui/collection/6fbcb4121a78476a77a6982498a1368624454c08bee9b597ba5d1ab1f1a06cbb'
+headers = {
+    "x-apikey": "6bf043675b60ab5626d10857b580e9456e415bbff9223f88258b77b2e85dc85b"
+}
+response = requests.post(upload_url, headers=headers)
+upload_data = response.json()
 
+print(response.text)
 # VirusTotal Instance
 virusTotal = virustotal_python.Virustotal(API_KEY)
 
@@ -16,34 +25,39 @@ apk_directory = r'C:\Users\jalee\PycharmProjects\APK'
 for filename in os.listdir(apk_directory):
     if filename.endswith('.apk'):
         apk_file_path = os.path.join(apk_directory, filename)
-
         try:
-            # Will open the file and read it
-            with open(apk_directory, 'rb') as file:
-                files = {'file' : (os.path.basename(apk_directory), file)}
-                response = virusTotal.request("files", files=files)
+            # Open the APK file for reading and prepare it for upload
+            with open(apk_file_path, 'rb') as apk_file:
+                files = {'file': (os.path.basename(apk_file_path), apk_file)}
+
+                # Make the POST request to upload the APK file using the provided upload URL
+                response = requests.post(upload_url, files=files, headers={"x-apikey": API_KEY})
 
                 if response.status_code == 200:
-                    # Will print the response
+                    # Get the response data and process it
                     response_data = response.json()
+                    resource_id = response_data.get('data', {}).get('id', '')
+                    print(f"APK file {filename} successfully uploaded. Resource ID: {resource_id}")
 
-                    if response_data.get('response_code') == 200:
-                        resource = response_data.get('resource')
-                        print(f"File {filename} successfully uploaded.")
-                        print(f"Scan ID: {resource}")
+                    # Add Resource ID to Collection
+                    collection_url = f"https://www.virustotal.com/api/v3/collections/{Collection_ID}/items"
+                    data = {
+                        "items": [
+                            {"type": "file", "id": resource_id}
+                        ]
+                    }
+                    collection_response = requests.post(collection_url, json=data, headers=headers)
+                    if collection_response.status_code == 200:
+                        print(f"Resource added to collection {Collection_ID}")
+                    else:
+                        print(f"Failed to add resource to collection {Collection_ID}")
 
-                        file_info_response = virusTotal.request("files/{resource}")
-                        file_info = file_info_response.data
-                        pprint(file_info)
                 else:
-                    print(f"File {filename} failed to upload.")
+                    print(f"Failed to upload APK file {filename}. Status code: {response.status_code}")
 
         except FileNotFoundError:
             print(f"File {filename} not found.")
-
         except requests.exceptions.RequestException as req_exc:
             print(f"Request failed: {req_exc}")
-
-        # All-rounder error type
         except Exception as e:
             print(f"Unknown error: {e}")
